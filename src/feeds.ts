deb07b072cc2b54d236f1a0947796efff8fbdaf9
IT-Service Börner <118393600+it-boerner@users.noreply.github.com>
2023-07-24 15:40:14 +0200

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,4 +1,7 @@
 export const feeds = [
   // Daring Fireball
   'https://www.umwelt.sachsen.de/umwelt/infosysteme/hwims/portal/web/feed/wasserstand-pegel-563780',
+
+  // Appjeniksaan
+  'https://www.umwelt.sachsen.de/umwelt/infosysteme/hwims/portal/web/feed/wasserstand-pegel-563780',
 ]
